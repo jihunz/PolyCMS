b72239fcbec180a198a39f263c6664f511a2ee5d
jihunz <thespringsong@gmail.com>
2022-03-21 15:46:06 +0900

    first commit

---
+++ src/main/java/kr/ac/kopo/dao/BoardDao.java
@@ -0,0 +1,23 @@
+package kr.ac.kopo.dao;
+
+import java.util.List;
+
+import kr.ac.kopo.model.Board;
+
+public interface BoardDao {
+
+	List<Board> list();
+
+	Board item(Long boardId);
+
+	void add(Board item);
+
+	void update(Board item);
+
+	void delete(Long boardId);
+
+	void createTable(Long boardId);
+
+	void deleteTable(Long boardId);
+
+}
